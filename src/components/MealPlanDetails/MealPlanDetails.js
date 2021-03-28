2a029d0ebb17011b3f92a57fa236eedfba491d5a
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2021-03-28 16:50:41 +0300

    Add details page for each meal

--- src/components/MealPlanDetails/MealPlanDetails.js
+++ src/components/MealPlanDetails/MealPlanDetails.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react';
 import * as mealPlanService from '../../services/mealPlansService'
+import './MealPlanDetails.css'
 const MealPlanDetails = ({match}) => {
 
     let [mealPlan, setMealPlan] = useState({});
@@ -15,15 +16,42 @@ const MealPlanDetails = ({match}) => {
        
     //     return;
     // }
-    
-    return ( 
-        <>
-        <h1>{mealPlan.name}</h1>
-        {Object.keys(mealPlan).length !== 0 && mealPlan.constructor === Object ? 
-         Object.keys(mealPlan.meals).map(x=> <div><p>{x}</p> - <p>{mealPlan.meals[x]}</p></div>) :
-          ""}
-        </>
-     );
+    if (Object.keys(mealPlan).length !== 0 && mealPlan.constructor === Object) {
+        return(
+            <div className="section background-white">
+        <section className="first-part ">
+            <h1>{mealPlan.name}</h1>
+            <img src={mealPlan.img} style={{width:"350px"}} alt=""/>
+        </section>
+        <hr className="cool-hr"/>
+        <section className="info">
+            <div>
+                <h1>Ingredients</h1>
+                {mealPlan.ingridients.map(x=>  <p key={x} className="ingridient">{x}</p>)}
+            </div>
+            
+                
+                <div className="description">
+                    <h1>How to cook</h1>
+                    <p>{mealPlan.description}</p>
+                </div>
+           
+        </section>
+
+        <div className="buttons">
+            <button className="form-btn">Edit</button>
+            <button className="form-btn">Delete</button>
+        </div>
+    </div>
+        )
+    } else {
+        
+        return ( 
+            <div className="section background-white">
+                <p style={{position:"relative", left:"49%", marginTop: "3%"}} className="three-quarters-loader">Loading...</p>
+            </div>
+         );
+    }
 }
  
 export default MealPlanDetails;
