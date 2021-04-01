54bd497ff504ab0ce72b4cef8ce61e213901f067
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2021-04-01 10:49:38 +0300

    Update AddMeal.js

--- src/components/AddMeal/AddMeal.js
+++ src/components/AddMeal/AddMeal.js
@@ -1,4 +1,4 @@
-import { Form, Formik } from "formik";
+import { Form, Formik, FieldArray, Field } from "formik";
 import * as Yup from 'yup';
 import CustomSelect from "../shared/CustomSelect";
 import CustomTextArea from "../shared/CustomTextArea";
@@ -15,10 +15,11 @@ const AddMeal = () => {
                             category: '',
                             calories: 0,
                             img:'',
+                            ingridients:[],
                         }}
                         validationSchema={Yup.object({
-                            name: Yup.string().min(3, 'At least 3 characters').max(15, 'Max 15 characters').required('Required'),
-                            description: Yup.string().min(10, 'At least 10 characters').max(1000, 'Max 1000 characters').required('Required'),
+                            name: Yup.string().min(3, 'At least 3 characters').max(25, 'Max 25 characters').required('Required'),
+                            description: Yup.string().min(10, 'At least 10 characters').required('Required'),
                             category: Yup.string().oneOf(['vegan', 'meat', 'vegatarian']).required("Required"),
                             calories: Yup.number().min(30,'Calories must be at least 30').max(1000, 'Calories can be max 1000').required("Required"),
                             img: Yup.string()
@@ -26,6 +27,8 @@ const AddMeal = () => {
                                 /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
                                 'Enter correct url!'
                             ).required('Required'),
+                            // ingridient:Yup.string().min(6, 'Ingridient and quantity must be at least 6 characters').max(20, 'Ingridient and quantity must be at least 20 characters')
+
                         })}
                         onSubmit={(values, { setSubmitting, resetForm }) => {
                             //service
@@ -50,6 +53,37 @@ const AddMeal = () => {
                                     <option value="vegatarian">Vegatarian</option>
                                 </CustomSelect>
                                 <CustomTextInput label="Image" name="img" type="text" placeholder="Image" />
+                                <FieldArray name='ingridients'   render={arrayHelpers => (
+              <div>
+                {props.values.ingridients && props.values.ingridients.length > 0 ? (
+                  props.values.ingridients.map((ingridient, index) => (
+                    <div key={index}>
+                      <Field name={`ingridients.${index}`} />
+                      <button
+                        type="button"
+                        onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                      >
+                        -
+                      </button>
+                    { props.values.ingridients.length - 1 === index && <button
+                        type="button"
+                        onClick={() => arrayHelpers.push("")} // insert an empty string at a position
+                      >
+                        +
+                      </button>}
+                    </div>
+                  ))
+                ) : (
+                   <button type="button" onClick={() => arrayHelpers.push("")}>
+                    {/* show this when user has removed all friends from the list */}
+                    Add an ingridient
+                  </button>
+                )}
+                <div>
+                </div>
+              </div>
+            )}
+          /> 
                                 <button type="submit">{props.isSubmitting ? 'Loading...' : 'Submit'}</button>
                             </Form>
                         )}
